e4055a5a85986a7d6dc5c35625e346e00d88efab
msdesai32 <msdesai32@gmail.com>
2024-05-06 11:28:02 +0530

    added file

---
+++ backend/predection.py
@@ -0,0 +1,47 @@
+from typing import List
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+
+class Plant():
+
+    def __init__(self, contents):
+        self.contents = contents
+
+        # Load the TensorFlow Lite model
+        self.interpreter = tf.lite.Interpreter(model_path="plant_tf_lite_model.tflite")
+        self.interpreter.allocate_tensors()
+
+        # Get input and output tensors
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+    def preprocess_image(self, image):
+        # Preprocess the image
+        img = image.convert("RGB").resize((256, 256))
+        img = np.array(img) / 255.0
+        img = img[np.newaxis, :, :, :].astype('float32')
+        return img
+
+    def predict_plant(self):
+        # contents = await file.read()
+      
+        image = Image.open(BytesIO(self.contents))
+
+        # Preprocess image
+        self.input_data = self.preprocess_image(image)
+
+        # Set input tensor
+        self.interpreter.set_tensor(self.input_details[0]['index'], self.input_data)
+
+        # Run inference
+        self.interpreter.invoke()
+
+        # Get output
+        output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+        prediction = np.argmax(output_data)
+        prediction = np.max(output_data)
+        print(output_data)
+        print("prediction", prediction)
+        return {"prediction": prediction}
--- backend/project.py
+++ backend/project.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from cropRecommend import Crop
-
-
+from predection import Plant
+# import numpy as np
 
 class CropSchema(BaseModel):
     Nitrogen: float
@@ -30,10 +30,11 @@ app.add_middleware(
 )
 
 
-
+ 
 
 @app.get('/')
 async def first_app():
+    print("hello world")
     return {'message': "Hello world"}
 
 Temperature=44
@@ -42,7 +43,7 @@ Humidity=66
 @app.post('/v2/api/crop') 
 async def crop(crop: CropSchema):
 
-    # print(crop.dict())  
+    print(crop.dict())  
     #get temp ,humidity from weather api
     data=Crop(crop.Nitrogen, crop.Phosphorus, crop.Potassium,Temperature,Humidity,crop.phValue, crop.Rainfall)
     output=data.Recommend()
@@ -53,19 +54,83 @@ async def crop(crop: CropSchema):
 
 
 
+# # 1st
+
+# # Load the TensorFlow Lite model
+# interpreter = tf.lite.Interpreter(model_path="your_model.tflite")
+# interpreter.allocate_tensors()
+
+# # Get input and output tensors
+# input_details = interpreter.get_input_details()
+# output_details = interpreter.get_output_details()
+
+# # Define labels
+# labels = ["class1", "class2", "class3"]  # Replace with your own labels
+
+
+# def preprocess_image(image):
+#     # Preprocess the image
+#     img = image.convert("RGB").resize((224, 224))
+#     img = np.array(img) / 255.0
+#     img = img[np.newaxis, :, :, :].astype('float32')
+#     return img
+
+
+
+
+
+
+
 
 @app.post("/v2/api/plant/upload/")
 async def plant_image(file: UploadFile):
+    print("hello world")
 
     if not file.filename.lower().endswith((".jpg", ".jpeg", ".png", ".gif")):
         return JSONResponse(content={"error": "Only image files are allowed."}, status_code=400)
 
 
-    with open(file.filename, "wb") as image_file:
-        image_file.write(file.file.read())
+    # with open(file.filename, "wb") as image_file:
+    #     image_file.write(file.file.read())
+
+    contents = await file.read()
+
+    plant= Plant(contents)
+    reult= plant.predict_plant()
+    print("prediction", reult['prediction'])
+    return JSONResponse(content={ "hello":'prediction'})
+
+# 2nd
+
+#   contents = await file.read()
+#     image = Image.open(BytesIO(contents))
+
+#     # Preprocess image
+#     input_data = preprocess_image(image)
+
+#     # Set input tensor
+#     interpreter.set_tensor(input_details[0]['index'], input_data)
+
+#     # Run inference
+#     interpreter.invoke()
+
+#     # Get output
+#     output_data = interpreter.get_tensor(output_details[0]['index'])
+#     prediction = np.argmax(output_data)
+
+#     return {"prediction": labels[prediction]}
 
 
-    return JSONResponse(content={"message": "Image uploaded successfully."})
+
+
+
+
+
+
+
+
+
+#     return JSONResponse(content={"message": "Image uploaded successfully."})
 
 
 
